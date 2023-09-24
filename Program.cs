a3fa9d8e8a7fd8e2f541438c5435e0079802cfdb
Stefeson <stefesongomesdealmeidanetoo@gmail.com>
2023-09-24 13:52:25 -0300

    Custo de Energia

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿//Stefeson
+
+double consumo, horasUsoDia;
+decimal custo, estimativaMes;
+
+Console.WriteLine("Custo de Energia");
+
+Console.Write("Consumo  de aparelho (em kWh/mês):");
+consumo = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Horas de uso por dia:");
+horasUsoDia = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Custo da energia (em R$/kWh):");
+custo = Convert.ToDecimal(Console.ReadLine());
+
+estimativaMes = Convert.ToDecimal(consumo * horasUsoDia) * custo;
+
+Console.WriteLine($"Custo estimado: {estimativaMes:C}");
